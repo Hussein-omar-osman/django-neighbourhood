681538b873945e789b9cc6137cfcd126f5ef77a8
Hussein Omar <husseinomar6190@gmail.com>
2022-06-18 21:45:35 +0300

    creating a Business model

--- base/models.py
+++ base/models.py
@@ -23,3 +23,16 @@ class NeighbourHood(models.Model):
       ordering = ['-created']
     def __str__(self):
         return self.name
+     
+class Business(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image')
+    contact = models.CharField(max_length=200)
+    neighbourHood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+      ordering = ['-created']
+    def __str__(self):
+        return self.name
